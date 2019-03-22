f2d685a7a5ada146329977d482022a8fbc4d3b45
LuisGasco <luisgasco@eprosima.com>
2019-03-22 12:08:26 +0100

    Refs #4972 Serialization of bitset field by field similary to an struct.

--- src/cpp/types/DynamicData.cpp
+++ src/cpp/types/DynamicData.cpp
@@ -5205,7 +5205,7 @@ bool DynamicData::deserialize(eprosima::fastcdr::Cdr& cdr)
         break;
     }
     case TK_STRUCTURE:
-    case TK_BITSET: // TODO Serializing as an structure, but should serialize only a primitive with declared bits.
+    case TK_BITSET:
     {
 #ifdef DYNAMIC_TYPES_CHECKING
         //uint32_t size(static_cast<uint32_t>(complex_values_.size())), memberId(MEMBER_ID_INVALID);
@@ -5600,7 +5600,7 @@ size_t DynamicData::getCdrSerializedSize(
         break;
     }
     case TK_STRUCTURE:
-    case TK_BITSET: // TODO Serializing as an structure, but should serialize only a primitive with declared bits.
+    case TK_BITSET:
     {
 #ifdef DYNAMIC_TYPES_CHECKING
         //for (auto it = data->complex_values_.begin(); it != data->complex_values_.end(); ++it)
@@ -5821,7 +5821,7 @@ size_t DynamicData::getMaxCdrSerializedSize(
         break;
     }
     case TK_STRUCTURE:
-    case TK_BITSET: // TODO Serializing as an structure, but should serialize only a primitive with declared bits.
+    case TK_BITSET:
     {
         for (auto it = type->member_by_id_.begin(); it != type->member_by_id_.end(); ++it)
         {
@@ -6101,7 +6101,7 @@ void DynamicData::serialize(eprosima::fastcdr::Cdr& cdr) const
         break;
     }
     case TK_STRUCTURE:
-    case TK_BITSET: // TODO Serializing as an structure, but should serialize only a primitive with declared bits.
+    case TK_BITSET:
     {
 #ifdef DYNAMIC_TYPES_CHECKING
         for (uint32_t idx = 0; idx < static_cast<uint32_t>(complex_values_.size()); ++idx)
@@ -6373,7 +6373,7 @@ size_t DynamicData::getEmptyCdrSerializedSize(
         break;
     }
     case TK_STRUCTURE:
-    case TK_BITSET: // TODO Serializing as an structure, but should serialize only a primitive with declared bits.
+    case TK_BITSET:
     {
         for (auto it = type->member_by_id_.begin(); it != type->member_by_id_.end(); ++it)
         {
@@ -6531,7 +6531,7 @@ void DynamicData::serialize_empty_data(
             break;
         }
         case TK_STRUCTURE:
-        case TK_BITSET: // TODO Serializing as an structure, but should serialize only a primitive with declared bits.
+        case TK_BITSET:
         {
             for (uint32_t idx = 0; idx < pType->member_by_id_.size(); ++idx)
             {
